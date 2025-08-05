194903dfeeb9d631bc5fb53f41cd668c3578ea6c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-05 10:53:10 +0900

    Getting the ML model and dataset from scikit-learn

--- supervised_learning/gradient_boosting_classifier.py
+++ supervised_learning/gradient_boosting_classifier.py
@@ -1,1 +1,9 @@
 # Ensemble learning algorithm
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
