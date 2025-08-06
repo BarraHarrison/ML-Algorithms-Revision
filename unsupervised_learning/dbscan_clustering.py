f4015b1808f2b60b38e7b79cf37473c9f96b530e
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-06 10:41:09 +0900

    Creating synthetic data and applying DBSCAN

--- unsupervised_learning/dbscan_clustering.py
+++ unsupervised_learning/dbscan_clustering.py
@@ -7,7 +7,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def run_dbscan():
-    pass
+    X, _ = make_moons(n_samples=300, noise=0.05, random_state=0)
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    dbscan = DBSCAN(eps=0.3, min_samples=5)
+    labels = dbscan.fit_predict(X_scaled)
+
+    
 
 if __name__ == "__main__":
     run_dbscan()
