dd99357d59fbec8e28cf4a4b087b6be8dba11a87
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-06 10:55:44 +0900

    Housing dataset and Linear Regression ML model from scikit-lern dependencies

---
+++ supervised_learning/linear_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+def run_linear_regression():
+    pass
+
+
+if __name__ == "__main__":
+    run_linear_regression()
