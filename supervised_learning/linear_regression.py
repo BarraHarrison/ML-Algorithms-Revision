77ba68f6ec4d2d9b626e4343983006a38705d190
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-06 11:01:57 +0900

    Loading the dataset and implementing the train/test split

--- supervised_learning/linear_regression.py
+++ supervised_learning/linear_regression.py
@@ -10,7 +10,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def run_linear_regression():
-    pass
+    data = fetch_california_housing()
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
 
 if __name__ == "__main__":
