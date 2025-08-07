af1d1631f68bc2010348541eb56e60ff7996e833
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-07 10:47:07 +0900

    Logistic regression predicts continous values instead of binary values like linear regression

---
+++ supervised_learning/logistic_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, roc_auc_score
+from sklearn.decomposition import PCA
+
+def run_logistic_regression():
+    pass
+
+if __name__ == "__main__":
+    run_logistic_regression()
