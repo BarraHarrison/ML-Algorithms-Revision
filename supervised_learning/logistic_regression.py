c4be73d06c79037749c62e1f4d9a983d67663c8c
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-07 10:50:46 +0900

    Train/Test split and model scaling

--- supervised_learning/logistic_regression.py
+++ supervised_learning/logistic_regression.py
@@ -11,7 +11,20 @@ from sklearn.metrics import classification_report, confusion_matrix, roc_curve, 
 from sklearn.decomposition import PCA
 
 def run_logistic_regression():
-    pass
+    data = load_breast_cancer()
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    model = LogisticRegression(max_iter=1000, solver="liblinear")
+    model.fit(X_train_scaled, y_train)
+
+    
 
 if __name__ == "__main__":
     run_logistic_regression()
