443b82e7cc1ccb323a7d73811be740ed018a1462
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-08 11:15:48 +0900

    KNN Decision Boundary

--- supervised_learning/knn_classifier.py
+++ supervised_learning/knn_classifier.py
@@ -56,6 +56,24 @@ def run_knn_classifier():
     knn_pca = KNeighborsClassifier(**grid.best_params_)
     knn_pca.fit(X_train, y_train)
 
+    h = 0.02
+    x_min, x_max = X_train_pca[:, 0].min() - 1, X_train_pca[:, 0].max() + 1
+    y_min, y_max = X_train_pca[:, 1].min() - 1, X_train_pca[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    Z = knn_pca.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(8, 6))
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.3)
+    plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=y_train, cmap=plt.cm.coolwarm, edgecolors='k', s=50)
+    plt.title("KNN Decision Boundary (PCA Projection)")
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     run_knn_classifier()
