4f1f68106896875dfc0e5adbaca512260e63fd62
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-08 10:57:31 +0900

    K-Nearest Neighbours looks at closest training points in the space

---
+++ supervised_learning/knn_classifier.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.decomposition import PCA
+
+def run_knn_classifier():
+    pass
+
+if __name__ == "__main__":
+    run_knn_classifier()
