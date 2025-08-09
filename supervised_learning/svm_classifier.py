7eddfeb9507d6aec26cd3aadd56d0541c47c5ef3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-09 17:58:32 +0900

    Splitting the data, standardize and the param grid

--- supervised_learning/svm_classifier.py
+++ supervised_learning/svm_classifier.py
@@ -13,3 +13,17 @@ X = iris.data[:, :2]
 y = iris.target
 X = X[y != 2]
 y = y[y != 2]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+param_grid = {
+    "C": [0.1, 1, 10, 100],
+    "degree": [2, 3, 4],
+    "coef0": [0, 0.5, 1],
+    "kernel": ["poly"]
+}
+
