9bdbfaf20abe2da73faf20f5cdc72fa83ac9d8a1
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-09 18:37:17 +0900

    TF-IDF Vectorization with Bigrams

--- supervised_learning/naive_bayes_classifier.py
+++ supervised_learning/naive_bayes_classifier.py
@@ -12,3 +12,12 @@ url = "https://raw.githubusercontent.com/justmarkham/scikit-learn-videos/master/
 df = pd.read_csv(url, sep="\t", header=None, names=["label", "message"])
 
 df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df["message"], df["label"], test_size=0.2, random_state=42, stratify=df["label"]
+)
+
+model = Pipeline({
+    ("tfidf", TfidfVectorizer(ngram_range=(1,2), stop_words="english", min_df=3)),
+    ("nb", MultinomialNB(alpha=0.5))
+})
