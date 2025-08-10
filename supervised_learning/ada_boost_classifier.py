e906562aadbb6b7302347d8f4542ea7c456ced66
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-10 14:22:05 +0900

    Ada Boost Model created and trained

--- supervised_learning/ada_boost_classifier.py
+++ supervised_learning/ada_boost_classifier.py
@@ -13,3 +13,14 @@ X = heart.data.features
 y = heart.data.targets.squeeze()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+base_estimator = DecisionTreeClassifier(max_depth=1, random_state=42)
+
+ada_model = AdaBoostClassifier(
+    base_estimator=base_estimator,
+    n_estimators=100,
+    learning_rate=0.5,
+    random_state=42
+)
+ada_model.fit(X_train, y_train)
+
