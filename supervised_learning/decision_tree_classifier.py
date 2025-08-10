fde5d1c7f011814105cf1dfd73d182a852e412e3
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-10 12:58:26 +0900

    Simulated dataset created

---
+++ supervised_learning/decision_tree_classifier.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib as plt 
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import classification_report, accuracy_score
+
+np.random.seed(42)
+data_size = 500
+
+data = pd.DataFrame({
+    "age": np.random.randint(30, 80, data_size),
+    "cholesterol": np.random.randint(150, 300, data_size),
+    "blood_pressure": np.random.randint(90, 180, data_size),
+    "smoker": np.random.choice([0, 1], size=data_size),
+    "diabetes": np.random.choice([0, 1], size=data_size)
+})
+
