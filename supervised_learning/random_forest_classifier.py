74dadf5d0bf294dc64cc92409dbefcc6920ce580
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-10 13:51:35 +0900

    Random Forest Classifier trained on the training data

--- supervised_learning/random_forest_classifier.py
+++ supervised_learning/random_forest_classifier.py
@@ -9,3 +9,12 @@ X = heart_disease.data.features
 y = heart_disease.data.targets
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+rf = RandomForestClassifier(
+    n_estimators=200,
+    max_features="sqrt",
+    bootstrap=True,
+    random_state=42
+)
+rf.fit(X_train, y_train)
+
