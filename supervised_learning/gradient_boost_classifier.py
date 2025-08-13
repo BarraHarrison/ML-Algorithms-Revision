077cd56d4fc2f10e983edbdb835c04a84ada027a
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-08-13 11:07:14 +0900

    Defining the gradient boost classifier and the parameter grid

--- supervised_learning/gradient_boost_classifier.py
+++ supervised_learning/gradient_boost_classifier.py
@@ -20,3 +20,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_imputed, y, test_size=0.2, random_state=42, stratify=y
 )
 
+gb = GradientBoostingClassifier(random_state=42)
+param_grid = {
+    "n_estimators": [100, 200],
+    "learning_rate": [0.05, 0.1, 0.2],
+    "max_depth": [2, 3, 4],
+    "subsample": [0.8, 1.0]
+}
+
